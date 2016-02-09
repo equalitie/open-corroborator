f54375e90edec419c030a13f024d9b4cd43a09e9
Greg Gaughan <greg.gaughan@isotoma.com>
2016-02-09 09:39:39 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+version = '0.1 demo'
+
+setup(
+    name='open-corroborator',
+    version=version,
+    long_description=open("README.md").read(),
+    packages=find_packages(exclude=['ez_setup']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'Django<1.6',
+        #todo 
+        #'South',
+
+        # Third Party Standard
+        #'gunicorn',
+    ],
+    entry_points = """
+    """
+    )
