4a13d2605fa1f84f73e8e7686a45f1cecd8b6f57
Greg Gaughan <greg.gaughan@isotoma.com>
2016-02-09 11:34:58 +0000

    Static paths

--- corroborator/settings/vm.py
+++ corroborator/settings/vm.py
@@ -1,5 +1,17 @@
 from dev import *
 
+DATABASES['default']['NAME'] = 'corroborator_vm'
+
 STATIC_ROOT = '/var/local/sites/open-corroborator-vm.stage.isotoma.com/var/static/'
 
-DATABASES['default']['NAME'] = 'corroborator_vm'
+CHECKOUT_ROOT = '/var/local/checkout/open-corroborator-vm.stage.isotoma.com/'
+STATICFILES_DIRS = (
+    ('stylesheets', os.path.join(CHECKOUT_ROOT, 'static/stylesheets')),
+    ('js', os.path.join(CHECKOUT_ROOT, 'static/js')),
+    ('images', os.path.join(CHECKOUT_ROOT, 'static/images')),
+    ('fonts', os.path.join(CHECKOUT_ROOT, 'static/fonts')),
+    ('css', os.path.join(CHECKOUT_ROOT, 'static/css')),
+    
+    ('admin', os.path.join(CHECKOUT_ROOT, 'static/admin')),
+)
+
