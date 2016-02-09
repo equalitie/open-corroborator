cda46e025e5867cf486944db073455cbfde0671f
Greg Gaughan <greg.gaughan@isotoma.com>
2016-02-09 11:36:35 +0000

    Typo

--- corroborator/settings/vm.py
+++ corroborator/settings/vm.py
@@ -4,7 +4,7 @@ DATABASES['default']['NAME'] = 'corroborator_vm'
 
 STATIC_ROOT = '/var/local/sites/open-corroborator-vm.stage.isotoma.com/var/static/'
 
-CHECKOUT_ROOT = '/var/local/checkout/open-corroborator-vm.stage.isotoma.com/'
+CHECKOUT_ROOT = '/var/local/checkouts/open-corroborator-vm.stage.isotoma.com/'
 STATICFILES_DIRS = (
     ('stylesheets', os.path.join(CHECKOUT_ROOT, 'static/stylesheets')),
     ('js', os.path.join(CHECKOUT_ROOT, 'static/js')),
