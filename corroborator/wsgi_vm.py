9776fb3124f77c43ae3a90a2ef8ba2144093f762
Greg Gaughan <greg.gaughan@isotoma.com>
2016-02-09 14:24:52 +0000

    wsgi paths

--- corroborator/wsgi_vm.py
+++ corroborator/wsgi_vm.py
@@ -4,13 +4,13 @@ WSGI config for open-corroborator project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.5/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
+sys.path.append("/var/local/sites/open-corroborator-vm.stage.isotoma.com/lib/python2.7/site-packages")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "corroborator.settings.vm")
 
 from django.core.wsgi import get_wsgi_application
-from keystone.celery_setup import app as _app
 
 application = get_wsgi_application()
