4caa22d9ecf42d61090341cc02f8fe43a1406966
Greg Gaughan <greg.gaughan@isotoma.com>
2016-02-22 14:07:10 +0000

    Simplify wsgi for scripted deployment

--- corroborator/wsgi.py
+++ corroborator/wsgi.py
@@ -1,53 +1,14 @@
 """
-WSGI config for corroborator project.
+WSGI config for open-corroborator project.
 
-This module contains the WSGI application used by Django's development server
-and any production WSGI deployments. It should expose a module-level variable
-named ``application``. Django's ``runserver`` and ``runfcgi`` commands discover
-this application via the ``WSGI_APPLICATION`` setting.
+It exposes the WSGI callable as a module-level variable named ``application``.
 
-Usually you will have the standard Django WSGI application here, but it also
-might make sense to replace the whole Django WSGI application with a custom one
-that later delegates to the Django one. For example, you could introduce WSGI
-middleware here, or combine a Django application with an application of another
-framework.
+For more information on this file, see
+https://docs.djangoproject.com/en/1.5/howto/deployment/wsgi/
 """
 
-# -*- coding: utf-8 -*-
-import os
-import site
-import sys
+#note: assumes caller has set DJANGO_SETTINGS_MODULE
 
+from django.core.wsgi import get_wsgi_application
 
-# Remember original sys.path.
-prev_sys_path = list(sys.path)
-
-# we add current directory to path and change to it
-pwd = os.path.dirname(os.path.abspath(__file__))
-os.chdir( pwd )
-sys.path = [pwd] + sys.path
-sys.path  = [pwd + '/../'] + sys.path
-
-# find the site-packages within the local virtualenv
-for python_dir in os.listdir('../env/lib'):
-    site_packages_dir = os.path.join("../env/lib", python_dir, "site-packages")
-    if os.path.exists(site_packages_dir):
-        site.addsitedir(os.path.abspath(site_packages_dir))
-
-# Reorder sys.path so new directories at the front.
-new_sys_path = []
-for item in list(sys.path):
-    if item not in prev_sys_path:
-        print item
-        new_sys_path.append(item)
-        sys.path.remove(item)
-        sys.path[:0] = new_sys_path
-
-
-# now start django
-#os.environ['DJANGO_SETTINGS_MODULE'] = 'settings.dev'
-#os.environ['DJANGO_SETTINGS_MODULE'] = 'settings.staging'
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings.demo'
-
-import django.core.handlers.wsgi
-application = django.core.handlers.wsgi.WSGIHandler()
+application = get_wsgi_application()
