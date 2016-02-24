c1761c3b9946c8f3e0f0a37d8db219ae6cdc3a04
Greg Gaughan <greg.gaughan@isotoma.com>
2016-02-24 10:54:47 +0000

    Update haystack connection to add core name

--- corroborator/settings/dev.py
+++ corroborator/settings/dev.py
@@ -95,8 +95,7 @@ HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.RealtimeSignalProcessor'
 HAYSTACK_CONNECTIONS = {
     'default': {
         'ENGINE': 'haystack.backends.solr_backend.SolrEngine',
-        'URL': 'http://127.0.0.1:8983/solr',
-        # ...or for multicore...
+        'URL': 'http://127.0.0.1:8983/solr/' + SOLR_CORE,
     },
 }
 
