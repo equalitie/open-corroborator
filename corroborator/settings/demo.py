82a18f2c4a0894b275f6636f619724b3b8022dbc
Greg Gaughan <greg.gaughan@isotoma.com>
2016-02-29 13:29:01 +0000

    Demo settings

---
+++ corroborator/settings/demo.py
@@ -0,0 +1,18 @@
+import sys
+from common import *
+
+ALLOWED_HOSTS = ['*']
+
+DATABASES['default']['NAME'] = 'corroborator_demo'
+
+QUEUED_STORAGE = False  #i.e. save uploads locally (in MEDIA_ROOT)
+SENDFILE_BACKEND = 'sendfile.backends.xsendfile'
+
+SITE_ROOT = sys.prefix
+
+MEDIA_ROOT = os.path.join(SITE_ROOT, 'var' ,'media')
+
+STATIC_ROOT = os.path.join(SITE_ROOT, 'static')
+STATICFILES_DIRS = ('/var/local/checkouts/corroborator/static',)
+
+TIME_ZONE = 'Europe/London'
