33ed434fd07e757e05f3f8f0f8650a7366bd0628
Greg Gaughan <greg.gaughan@isotoma.com>
2016-03-08 11:20:53 +0000

    Catch and log error if username not found
    (and avoid fetching SolrUpdate that won't then be used)

--- corroborator_app/tasks.py
+++ corroborator_app/tasks.py
@@ -2,6 +2,9 @@ from celery import task
 from haystack.management.commands import update_index
 from corroborator_app.models import SolrUpdate
 from django.contrib.auth.models import User
+from celery.utils.log import get_task_logger
+
+logger = get_task_logger(__name__)
 
 @task
 def update_object(username):
@@ -9,14 +12,17 @@ def update_object(username):
     options['age']=0.01
     options['remove']=True
     update_index.Command().handle(**options)
-    solrUpdateByUser = SolrUpdate.objects.all()
     user = User.objects.filter(username=username)
-    if len(solrUpdateByUser) == 0:
-        newUpdate = SolrUpdate(user=user[0])
-        newUpdate.save()
+    if user:
+        solrUpdateByUser = SolrUpdate.objects.all()
+        if len(solrUpdateByUser) == 0:
+            newUpdate = SolrUpdate(user=user[0])
+            newUpdate.save()
+        else:
+            solrUpdateByUser[0].user=user[0]
+            solrUpdateByUser[0].save()
     else:
-        solrUpdateByUser[0].user=user[0]
-        solrUpdateByUser[0].save()
+        logger.error("Unknown username: {} (SolrUpdate record not updated)".format(username))  #may not matter too much, since search polling seems to be off?
 @task
 def remove_object(index, instance, using=None):
     index.remove_object(instance, using=using)
