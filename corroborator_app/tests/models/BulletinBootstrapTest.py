5327ebcece06782787f020d67c700c363babd935
Greg Gaughan <greg.gaughan@isotoma.com>
2016-03-08 10:35:42 +0000

    Fix test result data

--- corroborator_app/tests/models/BulletinBootstrapTest.py
+++ corroborator_app/tests/models/BulletinBootstrapTest.py
@@ -36,7 +36,7 @@ class BulletinBootstrapTestCase(TestCase):
                 '/api/v1/source/1/',
                 '/api/v1/source/7/',
                 '/api/v1/source/9/'],
-            u'assigned_user': '/api/v1/user/2/',
+            u'assigned_user': '',
             u'actors_role': [
                 '/api/v1/actorRole/227/',
                 '/api/v1/actorRole/228/'],
@@ -58,8 +58,9 @@ class BulletinBootstrapTestCase(TestCase):
             u'type': u'Video', 'bulletin_labels': ['/api/v1/label/1/'],
             u'description_ar': u'', 'sources_count': 3,
             u'deleted': False,
-            u'title_en': u'IRX.A0001.648.001',
+            u'title_en': u'TEST.A0001.648.001',
             u'description_en': u'',
+            'id': 2,
             'bulletin_locations': [u'Homs'],
             u'seq_order': None,
             u'title_ar': u'',
@@ -76,7 +77,9 @@ class BulletinBootstrapTestCase(TestCase):
                 '/api/v1/incident/3/', '/api/v1/incident/4/',
                 '/api/v1/incident/6/', '/api/v1/incident/10/',
                 '/api/v1/incident/11/', '/api/v1/incident/12/',
-                '/api/v1/incident/13/']
+                '/api/v1/incident/13/'],
+            'resource_uri': '/api/v1/bulletin/2/'
         }
         bulletins = Bulletin.bootstrap_bulletins.filter(id=2)
+        self.maxDiff = None
         self.assertEqual(bulletins[0],  test_bulletin)
